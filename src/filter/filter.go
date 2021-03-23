eddd5f06a3db888e513a427d73b36f810236fb7e
jongsoo <jsoone24@gmail.com>
2021-03-23 22:39:33 +0900

    filter.go hello.go modifed

--- src/filter/filter.go
+++ src/filter/filter.go
@@ -1,28 +1,53 @@
 package main
 
-func filter(array []int, function func(x int, res []int)) []int {
-	result := []int{}
+import "fmt"
 
-	for i := 0; i < len(array); i++ {
-		function(array[i], result)
+func filter(array []int, fn func(e int) bool) []int {
+	var res []int = make([]int, len(array))
+
+	for i, v := range array {
+		if fn(v) {
+			res[i] = v
+		}
 	}
 
-	return result
+	return res
+}
+
+func mapFunc(array []int, fn func(int) int) []int {
+	for i, v := range array {
+		array[i] = fn(v)
+	}
+	return array
 }
 
 func main() {
 	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	filter(array, func(x int, res []int) {
-		if (x % 2) == 0 {
-			res = append(res, x)
-		}
+	/*
+		add := mapFunc(array, func(e int) int {
+			return e + 2
+		})
+
+		square := mapFunc(array, func(e int) int {
+			return e * e
+		})
+
+		filter(array, func(e int) bool {
+			return (e % 2) == 0
+		})
+
+		//or
+		filter(array, func(e int) bool {
+			return e < 5
+		})
+	*/
+
+	res := filter(mapFunc(array, func(e int) int {
+		return e + 10
+	}), func(e int) bool {
+		return (e % 5) < 2
 	})
 
-	//or
-	filter(array, func(x int, res []int) {
-		if x < 5 {
-			res = append(res, x)
-		}
-	})
+	fmt.Println(res)
 }
